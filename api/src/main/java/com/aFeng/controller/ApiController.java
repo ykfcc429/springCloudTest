d183eafc2e721a39ad96ab50bcb11a2e0c80bf61
1141154320@qq.com <Apple199773>
2020-07-19 00:51:42 +0800

    change response style

--- api/src/main/java/com/aFeng/controller/ApiController.java
+++ api/src/main/java/com/aFeng/controller/ApiController.java
@@ -14,12 +14,22 @@ public class ApiController {
     public synchronized String buy(){
         Jedis jedis = new Jedis();
         String mouse = jedis.get("mouse");
-        Long result = 0L,sales = 0L;
+//        Long result = 0L,sales = 0L;
         if(Integer.valueOf(mouse).equals(0)){
-            return "已经卖完了!";
+            return "已经卖完啦!";
         }
-        result = jedis.decr("mouse");
-        sales = jedis.incr("salesVolume");
-        return "you got No."+result+" mouse! your num is "+sales;
+//        result = jedis.decr("mouse");
+//        sales = jedis.incr("salesVolume");
+        return "<body style='background-color: black;font-size: 20;'>" +
+                "<div style='height: 50%;padding-top: 40%;padding-left: 19%;padding-right: 8%;color: orange;'>" +
+                "<h1 style='\n" +
+                "    color: white;\n" +
+                "    width: auto;\n" +
+                "    display: inline;\n'>Awesome! </h1><h1 style='\n" +
+                "    background-color: orange;\n" +
+                "    color: black;\n" +
+                "    width: auto;\n" +
+                "    display: inline-block;\n'>" +
+                "This is f**king crazy!</h1></div><body>";
     }
 }
