48dfb77a74e692e38f91ef1d6f5a2c697a8b2ab8
1141154320@qq.com <Apple199773>
2020-07-21 13:26:03 +0800

    商品信息在redis里改为使用hash存储,这样能够对单一字段进行修改,比如库存,价格,等,这样缓存信息非常方便且可靠 查询采用双检锁的形式进行,在redis没有数据的情况下只允许单个线程访问生产者数据库,在多个消费者服务的情况不可用,届时可采用redis分布式锁,也非常简单,但是对比双检锁有一定的死锁风险.单个应用场景下双检锁足够使用

--- consumer-80/src/main/java/com/aFeng/service/impl/GoodsServiceImpl.java
+++ consumer-80/src/main/java/com/aFeng/service/impl/GoodsServiceImpl.java
@@ -6,9 +6,9 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 import redis.clients.jedis.Jedis;
+import java.util.Map;
 
 @Service
 public class GoodsServiceImpl implements GoodsService {
@@ -30,14 +30,36 @@ public class GoodsServiceImpl implements GoodsService {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     * 双检锁,仅适用于单应用的前提下,如果redis中没有缓存数据,仅允许单个线程去访问生产者(数据库)
+     * 如果有多个消费者服务,应采用redis的分布式锁
+     * @param id 商品ID
+     * @return 商品信息的JSON字符串
+     */
     public Object findById(Long id) {
         Jedis jedis = redisUtil.getInstance();
-        String s = jedis.get("goods:" + id);
-        if(StringUtils.isEmpty(s)){
-            Object object = restTemplate.getForObject(URL+"/goods/get/"+id,Object.class);
-            s = JSON.toJSONString(object);
-            jedis.set("goods:"+id,s);
+        Map<String,String> map = jedis.hgetAll("goods:" + id);
+        String s;
+        if(map.isEmpty()){
+            synchronized (this){
+                if(map.isEmpty()){
+                    Object object = restTemplate.getForObject(URL+"/goods/get/"+id,Object.class);
+                    s = JSON.toJSONString(object);
+                    // redis hash本身只支持String类型的值
+                    //这里value虽然强转成String了,但还是属于原来的类,BigDecimal等无法转为String的类会在hSet的时候报错
+                    map = (Map<String, String>) JSON.parse(s);
+                    for(String key:map.keySet()){
+                        if(map.get(key)==null){
+                            map.put(key,"");
+                        }else {
+                            map.put(key, String.valueOf(map.get(key)));
+                        }
+                    }
+                    jedis.hmset("goods:"+id,map);
+                }
+            }
         }
+        s = JSON.toJSONString(map);
         jedis.close();
         return JSON.parse(s);
     }
