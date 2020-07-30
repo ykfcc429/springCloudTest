b3e2bac1323d2fc84be39f17c1c7ce466b8b135c
ykfcc429 <1141154320@qq.com>
2020-07-30 19:57:50 +0800

    created a useless method

--- consumer-80/src/main/java/com/aFeng/util/MapUtil.java
+++ consumer-80/src/main/java/com/aFeng/util/MapUtil.java
@@ -1,5 +1,8 @@
 package com.aFeng.util;
 
+import com.alibaba.fastjson.JSONObject;
+import com.sun.jersey.api.MessageException;
+import java.util.List;
 import java.util.Map;
 
 public class MapUtil {
@@ -13,6 +16,33 @@ public class MapUtil {
             map.putIfAbsent(key,"");
             map.put(key, String.valueOf(map.get(key)));
         }
+    }
+
+    /**
+     * 解决JSON俄罗斯套娃问题
+     * @param jsonObject 你需要处理的JsonObject
+     * @param keys 你需要取的key,请按顺序放
+     * @return keys最右边的key的值
+     */
+    public static JSONObject convertSomethingIGuess(JSONObject jsonObject, List<String> keys){
+        if(jsonObject==null){
+            throw new MessageException("就这?");
+        }
+        if(keys.isEmpty()){
+            throw new MessageException("不懂你什么意思");
+        }
+        if(jsonObject.containsKey(keys.get(0))){
+            JSONObject jsonObject1 = jsonObject.getJSONObject(keys.get(0));
+            if(jsonObject1==null){
+                throw new MessageException(keys.get(0)+" 的值为空了,无法继续获取值了");
+            }
+            if(keys.size()==1){
+                return jsonObject1;
+            }
+            keys.remove(0);
+            return convertSomethingIGuess(jsonObject1,keys);
+        }
+        throw new MessageException("很抱歉,看起来你没有 "+keys.get(0));
     }
 
 }
